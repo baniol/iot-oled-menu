963793aef5f76a30a6bbc09b99001f47e0f4413e
baniol <marcin.baniowski@gmail.com>
2018-11-08 22:38:44 +0100

    ttt

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(20, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-# GPIO.setup(21, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-# GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-# GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-# GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-# GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(20, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(21, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+GPIO.add_event_detect( 20, GPIO.FALLING, callback=trigger_1, bouncetime=300)
+
+def trigger_1():
+    print("20 ...\n")
 
 while True:
     time.sleep(5)
