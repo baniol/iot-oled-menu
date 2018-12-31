55517be869a70b43402d4990bdf86ff61aba9be3
baniol <marcin.baniowski@gmail.com>
2018-12-31 14:53:59 +0100

    teset changes

--- main.py
+++ main.py
@@ -7,15 +7,31 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(20, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(21, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(26, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(19, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
-GPIO.add_event_detect( 20, GPIO.FALLING, callback=trigger_1, bouncetime=300)
 
-def trigger_1():
+def trigger_20(pin):
     print("20 ...\n")
 
+def trigger_21(pin):
+    print("21 ...\n")
+
+def trigger_22(pin):
+    print("22 ...\n")
+
+def trigger_23(pin):
+    print("23 ...\n")
+
+def trigger_19(pin):
+    print("19 ...\n")
+
+GPIO.add_event_detect( 20, GPIO.FALLING, callback=trigger_20, bouncetime=300)
+GPIO.add_event_detect( 21, GPIO.FALLING, callback=trigger_21, bouncetime=300)
+GPIO.add_event_detect( 22, GPIO.FALLING, callback=trigger_22, bouncetime=300)
+GPIO.add_event_detect( 23, GPIO.FALLING, callback=trigger_23, bouncetime=300)
+GPIO.add_event_detect( 19, GPIO.FALLING, callback=trigger_19, bouncetime=300)
+
+
 while True:
-    time.sleep(5)
-    print("sleeping ...")
+    pass
